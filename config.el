386ac6cfaefee1c16d40b6fd317a878cf891974c
Noel Kwan <noelkwan1998@gmail.com>
2020-02-27 20:17:05 +0800

    Use org roam and associated packages

--- config.el
+++ config.el
@@ -1,4 +1,13 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(setq lsp-rust-server 'rust-analyzer)
+
+
+(load-theme 'doom-acario-light t)
+;; Show inline messages in org
+(defun my/fix-inline-images ()
+  (when org-inline-image-overlays
+    (org-redisplay-inline-images)))
+
+;; after executing show the images
+(add-hook 'org-babel-after-execute-hook 'my/fix-inline-images)
--- packages.el
+++ packages.el
@@ -1,7 +1,28 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; .doom.d/packages.el
+(package! org-roam :recipe
+  (:host github :repo "jethrokuan/org-roam" :branch "develop"))
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
+(use-package! org-roam
+      :hook
+      (after-init . org-roam-mode)
+      :custom (org-roam-directory "/home/noel/org-notes/")
+      :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n g" . org-roam-show-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))))
+
+;; Used to filter and browse files in org
+(package! deft)
+
+(use-package! deft
+  :after org
+  :bind
+  ("C-c n d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory "/home/noel/org-notes/"))
