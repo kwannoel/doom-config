2acccb408a851394cb661bdfff4c2adf801b24e8
Noel Kwan <noelkwan1998@gmail.com>
2020-03-24 14:42:47 +0800

    Update misc config

--- config.el
+++ config.el
@@ -4,7 +4,7 @@
 
 ;; Uses zsh instead of bash
 (setq vterm-shell "/run/current-system/sw/bin/zsh")
-(setq multi-term-program "/run/current-system/sw/bin/bash")
+(setq multi-term-program "/run/current-system/sw/bin/zsh")
 
 (load-theme 'doom-one t)
 ;; Show inline messages in org
@@ -19,3 +19,13 @@
 
 ;; after executing show the images
 (add-hook 'org-babel-after-execute-hook 'my/fix-inline-images)
+
+;; Haskell mode indentation
+(custom-set-variables
+ ;; Customization related to indentation.
+ '(haskell-indentation-layout-offset 4)
+ '(haskell-indentation-starter-offset 4)
+ '(haskell-indentation-left-offset 4)
+ '(haskell-indentation-where-pre-offset 2)
+ '(haskell-indentation-where-post-offset 2)
+ )
